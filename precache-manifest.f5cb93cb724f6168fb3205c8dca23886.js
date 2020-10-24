3cb46537d0e9196a606716285258abc303c6579b
Aamer Desai <56724460+minm333@users.noreply.github.com>
2020-10-24 19:15:36 -0400

    Updates

---
+++ precache-manifest.f5cb93cb724f6168fb3205c8dca23886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b8a4c84299ef786acaf020702794c6",
+    "url": "/react-lottery-game/index.html"
+  },
+  {
+    "revision": "9e82dc0306d11113e5d0",
+    "url": "/react-lottery-game/static/css/main.abe0f9b8.chunk.css"
+  },
+  {
+    "revision": "3cc4a856c28657b022ec",
+    "url": "/react-lottery-game/static/js/2.3987e965.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lottery-game/static/js/2.3987e965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e82dc0306d11113e5d0",
+    "url": "/react-lottery-game/static/js/main.261febf5.chunk.js"
+  },
+  {
+    "revision": "ee56dd386dba4ea9cb66",
+    "url": "/react-lottery-game/static/js/runtime-main.708cbee2.js"
+  }
+]);
